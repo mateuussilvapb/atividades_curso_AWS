b5a60d435ff87b5ad7d8d0bcea7cd57c9301dc8e
Mateus da Silva Sousa Dias <41194778+mateuussilvapb@users.noreply.github.com>
2024-07-31 10:42:26 -0300

    Create template_string.js

---
+++ template_string.js
@@ -0,0 +1,8 @@
+function operacoes(num1, num2) {
+  console.log(`${num1} + ${num2} = ${num1 + num2}`)
+  console.log(`${num1} - ${num2} = ${num1 - num2}`)
+  console.log(`${num1} / ${num2} = ${num1 / num2}`)
+  console.log(`${num1} * ${num2} = ${num1 * num2}`)
+}
+
+operacoes(5, 4)
